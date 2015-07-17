76c811bab706a11d96f90cb3bb5c9176cf3dda38
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-17 13:41:18 +0200

    New release of Sloth / Injector

--- deployToG5k.sh
+++ deployToG5k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -v -r $SLOTH_HOME/configuration .
+cp -v -r $SLOTH_HOME/configuration ./SLOTH_HOME
 cp -v $SLOTH_HOME/target/sloth.jar ./SLOTH_HOME/target
 cp -v $SLOTH_HOME/killNodes.py $SLOTH_HOME/runNodes.py $SLOTH_HOME/startNode.sh ./SLOTH_HOME
 
@@ -11,3 +11,9 @@ cp -v $INJECTOR_HOME/target/scala-2.10/dhtinjector.jar ./INJECTOR_HOME/target/sc
 git add .
 git commit -m "New release of Sloth / Injector"
 git push
+
+rsync -avrz -e ssh --progress . g5k:lyon/DHT-EXP
+
+
+
+
