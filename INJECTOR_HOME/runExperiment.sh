6f11d0181ac16b1d3e4b400e0355d55473060835
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-17 14:09:08 +0200

    New release of Sloth / Injector

--- INJECTOR_HOME/runExperiment.sh
+++ INJECTOR_HOME/runExperiment.sh
@@ -3,8 +3,8 @@
 : ${SLOTH_HOME?"The SLOTH_HOME environment variable should be set (please proceed: export SLOTH_HOME=...)"}
 : ${INJECTOR_HOME?"The INJECTOR_HOME environment variable should be set (please proceed: export INJECTOR_HOME=...)"}
 
-if [ $# -ne 4 ]; then 
-	echo "Usage: ./runExperiment.sh INJECTION_MODE NBNODE NODEFILE SERVICENODE"
+if [ $# -ne 3 ]; then 
+	echo "Usage: ./runExperiment.sh INJECTION_MODE NBNODE NODEFILE"
 	exit
 fi
 
@@ -14,14 +14,13 @@ echo "Experiment with Id: " $EXPERIMENTID
 INJECTION_MODE=$1
 NBNODE=$2
 ORIG_NODEFILE=$3 
-SERVICENODE=$4
 
 function executeExperiment {
     MODE=$1
-    echo "Starting ${MODE} Experiment"
+    echo "Starting ${MODE} Experiment with Id ${EXPERIMENTID}"
 
     NODEFILE=/tmp/$( basename $ORIG_NODEFILE)
-    head -n $((NBNODE)) $ORIG_NODEFILE > $NODEFILE
+    head -n $NBNODE $ORIG_NODEFILE > $NODEFILE
     # TODO check number of lines
     echo "Number of peers available $(wc -l $NODEFILE) (requested: $NBNODE)"
 
@@ -43,12 +42,20 @@ function executeExperiment {
 
     echo "  Start the injection phase"
     cd $INJECTOR_HOME
-    ./runInjector.py $NBNODE --nodes_address_file $NODEFILE --experimentId $EXPERIMENTID  --service_node $SERVICENODE
-   
-#    echo "Killing peers"
-#    cd $SLOTH_HOME
-#    ./killNodes.py --nodes_address_file $NODEFILE
-#
+    cp ./config/injector.properties ./config/injector.properties.orig
+    sed "s/peers.number.*/peers.number = ${NBNODE}/g" ./config/injector.properties > /tmp/injector.properties
+    cp /tmp/injector.properties ./config/injector.properties
+    sed "s/injection.mode.*/injection.mode = ${INJECTION_MODE}/g" ./config/injector.properties > /tmp/injector.properties
+    cp /tmp/injector.properties ./config/injector.properties
+    sed "s:dht.nodesaddress.*:dht.nodesaddress = "${NODEFILE}":g" ./config/injector.properties > /tmp/injector.properties
+    cp /tmp/injector.properties ./config/injector.properties
+
+    java -jar target/scala-2.10/dhtinjector.jar
+    
+    #echo "Killing peers"
+    #cd $SLOTH_HOME
+    #./killNodes.py --nodes_address_file $NODEFILE
+
 
     # Consolidate log files
     #echo "Consolidating log files"
@@ -58,8 +65,8 @@ function executeExperiment {
 }
 
 echo "Executing Eager Experiment"
-#executeExperiment eager > $INJECTOR_HOME/INJECTOR_${EXPERIMENTID}_EAGER.log
-executeExperiment eager 
+executeExperiment eager > $INJECTOR_HOME/INJECTOR_${EXPERIMENTID}_EAGER.log
+#executeExperiment eager 
 
 # echo "Executing Lazy Experiment"
 #executeExperiment lazy > $INJECTOR_HOME/INJECTOR_${EXPERIMENTID}_LAZY.log
