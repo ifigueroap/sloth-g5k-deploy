b94c9672790c657404d45f05f812fc8dfb0a4d88
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-17 13:44:05 +0200

    New release of Sloth / Injector

--- deployToG5k.sh
+++ deployToG5k.sh
@@ -12,7 +12,7 @@ git add .
 git commit -m "New release of Sloth / Injector"
 git push
 
-rsync -avrzd -e ssh --progress . g5k:lyon/DHT-EXP
+rsync -avrzd -e ssh --progress --exclude-from rsync-exclude . g5k:lyon/DHT-EXP
 
 
 
