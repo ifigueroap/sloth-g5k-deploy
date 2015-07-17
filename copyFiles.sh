37266ee20740944881574720391da5ab161be6a5
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-17 19:06:49 +0200

    New release of Sloth / Injector

--- copyFiles.sh
+++ copyFiles.sh
@@ -4,8 +4,8 @@
 # -v : verbose
 # -p : keep "last modified time" attribute of files. Otherwise rsync will think all files are 'new'
 
-cp -v -p -r $SLOTH_HOME/configuration ./SLOTH_HOME
-cp -v -p $SLOTH_HOME/target/sloth.jar ./SLOTH_HOME/target
+cp -v -p -r $SLOTH_HOME_DEV/configuration ./SLOTH_HOME
+cp -v -p $SLOTH_HOME_DEV/target/sloth.jar ./SLOTH_HOME/target
 
-cp -v -p -r $INJECTOR_HOME/config ./INJECTOR_HOME
-cp -v -p $INJECTOR_HOME/target/scala-2.10/dhtinjector.jar ./INJECTOR_HOME/target/scala-2.10/
+cp -v -p -r $INJECTOR_HOME_DEV/config ./INJECTOR_HOME
+cp -v -p $INJECTOR_HOME_DEV/target/scala-2.10/dhtinjector.jar ./INJECTOR_HOME/target/scala-2.10/
