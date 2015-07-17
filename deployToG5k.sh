472ab854ac10e295b786da705307947d3e62f4ad
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-17 16:41:38 +0200

    Script...

--- deployToG5k.sh
+++ deployToG5k.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-# -v : verbose
-# -p : keep "last modified time" attribute of files. Otherwise rsync will think all files are 'new'
-
-cp -v -p -r $SLOTH_HOME/configuration ./SLOTH_HOME
-cp -v -p $SLOTH_HOME/target/sloth.jar ./SLOTH_HOME/target
-
-cp -v -p -r $INJECTOR_HOME/config ./INJECTOR_HOME
-cp -v -p $INJECTOR_HOME/target/scala-2.10/dhtinjector.jar ./INJECTOR_HOME/target/scala-2.10/
+./copyFiles
 
 git add .
 git commit -m "New release of Sloth / Injector"
