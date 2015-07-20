fd31aa5d2406a62562a28058384d7fbfd3b36a68
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-20 17:52:29 +0200

    Avoids redundant pkills in hosts

--- SLOTH_HOME/killNodes.py
+++ SLOTH_HOME/killNodes.py
@@ -13,7 +13,9 @@ parser.add_argument('-f', '--nodes_address_file', dest='nodes_address_file', hel
 args = parser.parse_args()
 
 # retrieve the list of hosts from the file
-hosts = [line.strip().split(':')[0] for line in open(args.nodes_address_file)]
+# retrieve the list of hosts from the file
+hosts = list(set([line.strip().split(':')[0] for line in open(args.nodes_address_file)]))
+print "Killing hosts" + str(hosts)
 
 logger.info('kill java sloth peers')
 TaktukRemote('pkill -9 -f sloth.jar ; rm /tmp/sloth_launcher*', hosts).run()
