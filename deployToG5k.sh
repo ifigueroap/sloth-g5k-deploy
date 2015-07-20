f85e898df6287845783dc32adac1bea3b9f837af
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-20 18:39:41 +0200

    New release of Sloth / Injector

--- deployToG5k.sh
+++ deployToG5k.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+THISDIR=$PWD
+
+echo "### sbt assembly SLOTH ###"
+cd $SLOTH_HOME_DEV
+sbt assembly
+
+echo "### sbt assembly INJECTOR ###"
+cd $INJECTOR_HOME_DEV
+sbt assembly
+
+echo "Copying files..."
+cd $THISDIR
 ./copyFiles.sh
 
+echo "Adding to git..."
 git add .
 git commit -m "New release of Sloth / Injector"
 git push
 
+echo "rsyncing to g5k:lyon/DHT-EXP..."
 rsync -avrzd -e ssh --progress --exclude-from rsync-exclude . g5k:lyon/DHT-EXP
 
+echo "Done!"
 
 
 
