9a2e4d6dd507dfbb6fd37ae5063db33887b60da1
Ismael Figueroa <ifigueroap@gmail.com>
2015-07-27 11:36:17 -0300

    New release of Sloth / Injector

--- deployToG5k.sh
+++ deployToG5k.sh
@@ -18,14 +18,30 @@ fi
 
 echo "Copying files..."
 cd $THISDIR
-./copyFiles.sh
+# -v : verbose
+# -p : keep "last modified time" attribute of files. Otherwise rsync will think all files are 'new'
 
-echo "Adding to git..."
-git add .
-git commit -m "New release of Sloth / Injector"
-git push
+cp -v -p -r $SLOTH_HOME_DEV/configuration ./SLOTH_HOME
+cp -v -p $SLOTH_HOME_DEV/target/sloth.jar ./SLOTH_HOME/target
+
+cp -v -p -r $INJECTOR_HOME_DEV/config ./INJECTOR_HOME
+cp -v -p $INJECTOR_HOME_DEV/target/scala-2.10/dhtinjector.jar ./INJECTOR_HOME/target/scala-2.10/
+
+
+read -p "Push to github?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   # do dangerous stuff
+   echo "Adding to git..."
+   git add .
+   git commit -m "New release of Sloth / Injector"
+   git push
+fi
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
 echo "Done!"
-
-
 
